6ba475fbf2ab284add89b4872a5c2a58636eacfb
jakeportaro <jakeportaro@gmail.com>
2022-09-01 14:06:28 -0700

    added express code and updated minor fixes

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require ('express');
+const app = express();
+
+
+let topMovies = [
+    {
+    Name: "in Progress"
+    }
+];
+
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+  });
+
+  app.get('/', (req, res) => {
+    res.send('This Movie Collection will blow your mind!');
+  });
+
+  app.use('/documentation.html', express.static('public'));
+
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something isn"t loading right :(!');
+  });
