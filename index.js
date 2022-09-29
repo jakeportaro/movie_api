caccf73a824433011a5794f8d5f73fd56250281a
jakeportaro <jakeportaro@gmail.com>
2022-09-28 17:41:19 -0700

    taking out Passport Auth

--- index.js
+++ index.js
@@ -63,9 +63,7 @@ let movies = [
 
 // Gets the list of data about ALL movies
 
-app.get("/movies",
-passport.authenticate('jwt', { session: false }),
-(req, res) => {
+app.get("/movies", (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
